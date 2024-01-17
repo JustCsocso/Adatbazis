ad1c1e2c4e5c0a28f9ab4ec7d458bc959812d96f
Pintér Csongor <pinterc21@ADY-NAGYATAD.SULINET.HU>
2024-01-17 14:04:29 +0100

    dw

---
+++ xampp gyakolas/gyakorlas1219.sql
@@ -0,0 +1,33 @@
+DROP DATABASE nyiltnap;
+CREATE DATABASE nyiltnap DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+USE nyiltnap;
+
+CREATE TABLE tantargy (id int(10) AUTO_INCREMENT PRIMARY KEY,nev VARCHAR(50)); 
+
+CREATE TABLE oktato (id int(10) AUTO_INCREMENT PRIMARY KEY,nev VARCHAR(50)); 
+
+CREATE TABLE agazat (id int(10) AUTO_INCREMENT PRIMARY KEY,nev VARCHAR(50)); 
+
+
+CREATE TABLE oktato2tantargy (id int(10) AUTO_INCREMENT PRIMARY KEY,oktatoid INT, tantargyid INT);
+
+CREATE TABLE osztaly2agazat (id int(10) AUTO_INCREMENT PRIMARY KEY,osztalyid INT, agazatid INT); 
+
+CREATE TABLE osztaly (id int(10) AUTO_INCREMENT PRIMARY KEY,evfolyam INT,betujel VARCHAR(10), osztalyfonokid INT));
+
+CREATE TABLE idopont (id int(10) AUTO_INCREMENT PRIMARY KEY,datum DATE,ora INT, osztalyid INT, oktato2tantargyid INT);
+
+Óra Osztály/csoport	ágazat/tagozat	tantárgy	oktató	osztályfőnök	dátum
+CREATE TABLE eredeti(
+	id int(10) AUTO_INCREMENT primary key,
+	ora int(10),
+	osztaly varchar(50),
+	agazat varchar(50),
+	tantargy varchar(50),
+	oktato varchar(50),
+	ofo varchar(50),
+	dátum DATE
+);
+
+INSERT INTO eredeti(ora,osztaly,agazat,tantargy,oktato,ofo,datum);
